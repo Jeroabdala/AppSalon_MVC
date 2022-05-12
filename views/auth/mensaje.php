86ee887d0a8cabf30c0530875ebf092f5be1b3b1
Jeronimo Abdala <jeroabdalala@gmail.com>
2022-05-11 21:01:42 -0300

    bien

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,3 +1,6 @@
 <h1 class="nombre-pagina">Confirma tu cuenta</h1>
 
 <p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar tu cuenta a tu e-mail.</p>
+<p class="descripcion-pagina">Actualmente la funcion para validar email esta inactiva por cuestion de que lo realizo por MailTrap y no por un host "real"</p>
+
+<a href="/">Iniciar Sesion</a>
